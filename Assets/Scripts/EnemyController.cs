f20819eb33affbe4dc9517d94c53a03f42766329
SecureB00t <ashercoates@proton.me>
2026-08-26 10:33:03 -0500

    Revert "Reapply "Reapply "Reapply "Reapply "octorok basic behavior"""""
    This reverts commit 5c5e198ae6998bc82110677030087759db3d88d4.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
-using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] protected int HP = 1;
-    private Coroutine flashCoroutine;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Weapon") && flashCoroutine == null)
+        if (other.CompareTag("Weapon"))
         {
-            Flash();
             HP--;
             if (HP <= 0)
             {
@@ -28,31 +25,5 @@ public class EnemyController : MonoBehaviour
     public virtual void MoveEnemy()
     {
        // Debug.Log("Enemy is moving.");
-    }
-
-    public void Flash()
-    {
-        flashCoroutine = StartCoroutine(FlashRoutine());
-    }
-
-    IEnumerator FlashRoutine()
-    {
-
-        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-        Color originalColor = spriteRenderer.color;
-        Color flashColor = Color.red;
-
-        float flashDuration = 0.1f; // Duration of each flash
-        int flashCount = 3; // Number of flashes
-
-        for (int i = 0; i < flashCount; i++)
-        {
-            spriteRenderer.color = flashColor;
-            yield return new WaitForSeconds(flashDuration);
-            spriteRenderer.color = originalColor;
-            yield return new WaitForSeconds(flashDuration);
-        }
-
-        flashCoroutine = null;
     }
 }
