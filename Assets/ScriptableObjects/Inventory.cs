48032bb63779f8fa1c330485806c7dc2c6e637e6
SecureB00t <ashercoates@proton.me>
2026-08-26 10:33:05 -0500

    Revert "Reapply "Reapply "Reapply "Reapply "Added Initial Bomb-inventory logic"""""
    This reverts commit 11368614f610cf7b7d8d9cf2213225e83b378a57.

--- Assets/ScriptableObjects/Inventory.cs
+++ Assets/ScriptableObjects/Inventory.cs
@@ -15,43 +15,6 @@ public class Inventory : ScriptableObject
     {
         items.Remove(itemToRemove);
     }
-
-    public ItemInstance GetItem(ItemData itemData)
-    {
-        return items.Find(item => item.itemType == itemData);
-    }
-
-    public void AddAmmo(ItemData itemData, int amount)
-    {
-        ItemInstance itemInstance = GetItem(itemData);
-        if (itemInstance != null)
-        {
-            itemInstance.ammo += amount;
-        }
-    }
-
-    public void RemoveAmmo(ItemData itemData, int amount)
-    {
-        ItemInstance itemInstance = GetItem(itemData);
-        if (itemInstance != null && itemInstance.ammo >= amount)
-        {
-            itemInstance.ammo -= amount;
-
-        }
-    }
-
-
-    public int GetAmmo(ItemData itemData)
-    {
-        ItemInstance itemInstance = GetItem(itemData);
-
-        if (itemInstance != null)
-        {
-            return itemInstance.ammo;
-        }
-
-        return 0;
-    }
 }
 
 
