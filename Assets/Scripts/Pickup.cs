605eb22b052051f2fa71a8d563828d6e256b081c
SecureB00t <ashercoates@proton.me>
2026-08-26 10:31:41 -0500

    Revert "Reapply "Reapply "refactor start"""
    This reverts commit 4266106242045a1a46d8450249dcfffd34957074.

--- Assets/Scripts/Pickup.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Pickup : MonoBehaviour
-{
-    [SerializeField] public ItemData itemData;  //what kind of item it is
-    [SerializeField] public int ammoAmount = 1; //how much to add
-}
