a4c74649ea1623cf31ce322cde774f8b713d751a
SecureB00t <ashercoates@proton.me>
2026-08-26 10:32:40 -0500

    Revert "Reapply "Reapply "Reapply "Reapply "separated sword animation"""""
    This reverts commit 2a448e05f5b258a0eda386a8d006b5dc546a8370.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,17 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    [SerializeField] private BoxCollider2D swordCollider;
 
     private Vector2 input;
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer spriteRenderer;
     private Coroutine flipCoroutine;
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        spriteRenderer = transform.Find("Sprite").GetComponent<SpriteRenderer>();
-        // Automatically find the sword collider
-        swordCollider = transform.Find("Weapon/Sword").GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -30,27 +26,22 @@ public class PlayerController : MonoBehaviour
         myAnimator.SetFloat("Vertical", input.y);
         myAnimator.SetBool("isMoving", input != Vector2.zero);
 
-        if (input != Vector2.zero)
-        {
-            myAnimator.SetFloat("LastHorizontal", input.x);
-            myAnimator.SetFloat("LastVertical", input.y);
+        if (input.x > 0){
+            spriteRenderer.flipX = true;
+        }
+        else if (input.x < 0){
+            spriteRenderer.flipX = false;
         }
 
         if (input.y != 0 && flipCoroutine == null)
         {
             flipCoroutine = StartCoroutine(walkAnimationFlipTimerTrue(.1f));
         }
-        else if (input.y == 0 && flipCoroutine != null)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f); // Reset scale when not moving vertically
+
+        if(input != Vector2.zero){
+            myAnimator.SetFloat("LastHorizontal", input.x);
+            myAnimator.SetFloat("LastVertical", input.y);
         }
-
-        if (input.x != 0)
-        {
-            transform.localScale = new Vector3(-Mathf.Sign(input.x), 1f, 1f); // Flip sprite based on horizontal input
-        }
-
-
 
     }
 
@@ -62,58 +53,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
-    private void FixedUpdate()
-    {
-        if (input != Vector2.zero)
-        {
-            rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
-        }
-    }
-
-
     private IEnumerator walkAnimationFlipTimerTrue(float timeToWait)
     {
         Debug.Log(input.y);
-
-
         while(input.y != 0){
-            //spriteRenderer.flipX = true;                      FLIP EVERYTHING
-            //yield return new WaitForSeconds(timeToWait);      WAIT
-            //if(input.y==0){break;}                            STOP IF NO INPUT
-            //spriteRenderer.flipX = false;                     FLIP BACK
-            //yield return new WaitForSeconds(timeToWait);      WAIT
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            spriteRenderer.flipX = true;
             yield return new WaitForSeconds(timeToWait);
-            if(input.y==0){break;}                            
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            if(input.y==0){break;}         
+            spriteRenderer.flipX = false;
             yield return new WaitForSeconds(timeToWait);
         }
 
         if(input == Vector2.zero){
-            //spriteRenderer.flipX = false;                     RESET IF NO INPUT
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            spriteRenderer.flipX = false;
         }
-
-
 
         flipCoroutine = null;
 
     }
 
-    private void OnDrawGizmos()
+    void Update()
     {
-        if (swordCollider == null)
-            return;
-
-        Gizmos.color = swordCollider.enabled ? Color.red : Color.gray;
-
-        Vector3 center = swordCollider.transform.TransformPoint(swordCollider.offset);
-        Vector3 size = Vector3.Scale(
-            swordCollider.size,
-            swordCollider.transform.lossyScale
-        );
-
-        Gizmos.DrawWireCube(center, size);
+        rb.MovePosition(rb.position + input * (speed * Time.fixedDeltaTime));
+        Debug.Log(spriteRenderer.flipX);
     }
 }
