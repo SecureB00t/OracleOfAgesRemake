140dd894837d64f8c7c23d4b24a2582970e371e3
SecureB00t <ashercoates@proton.me>
2026-09-12 11:23:34 -0500

    Cutscene fully done with paused time, following, and animations

--- Assets/Scripts/GameManagement/DialogueHandler.cs
+++ Assets/Scripts/GameManagement/DialogueHandler.cs
@@ -57,73 +57,6 @@ public class DialogueHandler : MonoBehaviour
             }
         }
 
-        
-        // if (dialogueFinished){
-        //     dialogueFinished = false;
-        //     speakingNPC = npc;
-        //     currentMessage = dialogue.start;
-        //     dialogueContext = new DialogueContext(speakingNPC, this);
-        // }
-
-        // if (dialogueStart == null)
-        // {
-        //     dialogueStart = dialogue.start;
-        // }
-
-        // if (isTyping){
-        //     StopCoroutine(typewriter);
-        //     textMeshPro.maxVisibleCharacters = message.text.Length;
-        //     isTyping = false;
-        //     textMeshPro.text = message.text;
-        //     return;
-        // }
-
-        // if(dialogueFinished){ //Go to start of message if no dialogue is displayed (Bad approach but whatever)
-        //     dialogueFinished = false;
-        //     speakingNPC = npc;
-        //     currentMessage = dialogueStart;
-        //     dialogueContext = new DialogueContext(speakingNPC, this);
-        //     //currentMessage = dialogue.start;
-        // }
-
-        // else{ //Find the next message from the current message (I don't understand lambda functions)
-        //     message = dialogue.messages.Find(m => m.id == currentMessage);
-        //     currentMessage = message.next;
-        // }
-
-        // message = dialogue.messages.Find(m => m.id == currentMessage); //Set the actual current message
-
-
-        // if (message != null){
-        //     if (message.continuePoint != -1)
-        //     {
-        //         dialogueStart = message.continuePoint;
-        //     }
-        // }
-        // if (currentMessage == -1)
-        // {
-        //     dialogueBox.SetActive(false);
-        //     inputController.ResumePlayerMovement();
-        //     speakingNPC = null;
-        //     dialogueFinished = true;
-
-        // }
-
-
-        // else if (!isTyping)
-        // { //Display next message
-        //     foreach(DialogueAction action in message.actions)
-        //     {
-        //         action.Execute(dialogueContext);
-        //     }
-        //     dialogueBox.SetActive(true);
-        //     typewriter= StartCoroutine(TypewriterEffect(message.text));
-        // }
-
-    
-
-
-
 
     private IEnumerator TypewriterEffect(string line)
     {
@@ -138,7 +71,7 @@ public class DialogueHandler : MonoBehaviour
         while (visibleCharacters < line.Length)
         {
             isTyping = true;
-            timer += Time.deltaTime;                            //I think this is clever. Time.deltaTime is synced to real seconds. Not framerate. 
+            timer += Time.unscaledDeltaTime;                            //I think this is clever. Time.deltaTime is synced to real seconds. Not framerate. 
                                                                 //We run the timer and when it is greater than our chars per second, we subtract the timing of chars per second to make sure
             if (timer >= interval)                              //we play catchup if needed.
             {                                                   //eg. T=0, i = .1. T=0>T=.5>T=.1> T=0
@@ -157,6 +90,7 @@ public class DialogueHandler : MonoBehaviour
 
     private void StartDialogue(NPCController npc, Dialogue dialogue){
         //start dialogue
+        Time.timeScale = 0f;
         dialogueStarted = true;
         speakingNPC = npc;
         dialogueStartPoint = npc.dialogueStartPoint;
@@ -168,14 +102,15 @@ public class DialogueHandler : MonoBehaviour
 
     private void EndDialogue(){
         //end dialogue
+        Time.timeScale = 1f;
         dialogueStarted = false;
         dialogueStartPoint = null;
         speakingNPC = null;
         currentMessage = null;
-        dialogueContext = null;
         dialogueBox.SetActive(false);
         ExecuteDialogueActions();
         inputController.ResumePlayerMovement();
+        dialogueContext = null;
     }
 
     private void NextMessage(NPCController npc, Dialogue dialogue){
